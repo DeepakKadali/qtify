8d80927b55792cf16507e16abbe17fa5345d6e4e
DeepakKadali <deepakkadali03@gmail.com>
2023-12-01 11:45:33 +0530

    Module2 Albums

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import HeroSection from "./components/HeroSection/HeroSection";
-import Section from "./components/Section/Section"
+import TopAlbums from "./components/TopAlbums/TopAlbums";
+import NewAlbums from "./components/NewAlbums/NewAlbums";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <HeroSection />
-      <Section />
+      <TopAlbums />
+      <NewAlbums />
     </div>
   );
 }
